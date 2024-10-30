e259148a0f14bf3beb7a361b73d86ed50e1a6a41
Shiyi <scheng1@upv.edu.es>
2024-10-30 10:31:32 +0100

    Estructura json correcto

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ COPY requirements.txt ./
 COPY main.py ./
 
 RUN pip install -r requirements.txt
-RUN pip install google-cloud-bigquery google-cloud-bigquery-storage vertexai pandas google-cloud-aiplatform db-dtypes google-cloud-bigquery-storage
+RUN pip install flask_cors google-cloud-bigquery google-cloud-bigquery-storage vertexai pandas google-cloud-aiplatform db-dtypes google-cloud-bigquery-storage
 
 EXPOSE 8080
 
--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import vertexai
 from vertexai.language_models import TextEmbeddingModel
 from vertexai.preview.generative_models import GenerativeModel
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+import hashlib
 
 # Initialize Vertex AI
 vertexai.init(project="dataton-2024-team-11-cofares", location="europe-west1")
@@ -33,7 +35,7 @@ def gen_conv_KNN(conv: List[Tuple[str, str]], veto: List[productos]) -> str:
     return prompt
 
 def query_template(data):
-    return "SELECT base.id, distance FROM VECTOR_SEARCH(TABLE DataHub.Muestras_Cofares_Limpio_Emb,'ml_generate_embedding_result', ((select " + data + " as ml_generate_embedding_result)),top_k => 5,distance_type => 'COSINE');"
+    return "with VectorSearchResults AS (SELECT base.id, distance FROM VECTOR_SEARCH(TABLE DataHub.embeddings_cervantes,'embed', ((select " + data + " as embed)),top_k => 5,distance_type => 'COSINE')) SELECT search.id, search.distance, DC.nombre_material, DC.txt_mas_informacion_del_producto, DC.ids_imagenes, concat('nombre ', ifnull(DC.nombre_material,''),'.Nombre_largo ', ifnull(DC.nombre_material_largo,''),'.Marca propia ', DC.es_marca_propia,'.Nombre proveedor ', ifnull(DC.nombre_proveedor,''),'.Nombres matrículas: ',ifnull(DC.nombre_matricula_nivel0,''), ', ',ifnull(DC.nombre_matricula_nivel1,''), ', ',ifnull(DC.nombre_matricula_nivel2,''), ', ',ifnull(DC.nombre_matricula_nivel3,''), ', ',ifnull(DC.nombre_matricula_nivel4,''), ', ',ifnull(DC.nombre_matricula_nivel5,''),'. Información ', ifnull(DC.txt_mas_informacion_del_producto,''),'. Instrucciones ', ifnull(DC.txt_instrucciones_de_uso,''),'. Composición ', ifnull(DC.txt_composicion,'')) AS content FROM DataHub.Datos_Cofares AS DC JOIN  VectorSearchResults AS search ON DC.codigo_material = search.id"
 
 def buscar_cercanos(data, veto: List[str]) -> pd.DataFrame:
     client = bigquery.Client()
@@ -42,10 +44,9 @@ def buscar_cercanos(data, veto: List[str]) -> pd.DataFrame:
     emb = query_template(str(embeddings[0].values))
 
     query_job = client.query(emb)
-    results = query_job.result()
+    df = query_job.result().to_dataframe()
 
-    # Convertir los resultados a un DataFrame
-    df = results.to_dataframe()
+    contexto = str(df["content"])
 
     # Calculate similarity (1 - distance)
     df['similarity'] = 1 - df['distance']
@@ -55,7 +56,20 @@ def buscar_cercanos(data, veto: List[str]) -> pd.DataFrame:
         if x in veto:
             df.drop(n)
     df = df.sort_values('similarity', ascending=False)
-    return df
+
+    productos_encontrados = []
+    for _, row in df.iterrows():
+        producto_info = {
+            "id": row["id"],
+            "Nombre": row["nombre_material"],
+            "Descripcion": row["txt_mas_informacion_del_producto"],
+            "Similitud": row["similarity"],
+            "Imagenes": str(row["ids_imagenes"]).split(",") if row["ids_imagenes"] else []
+        }
+        productos_encontrados.append(producto_info)
+
+    client.close()
+    return productos_encontrados, contexto
 
 def get_info(df):
     contexto = ""
@@ -65,15 +79,15 @@ def get_info(df):
         consulta += "'" + df["id"][x] + "',"
     consulta = consulta[:-1]
 
-    query = "SELECT nombre_material,concat('nombre ', ifnull(nombre_material,''), '.Nombre_largo ', ifnull(nombre_material_largo,''),'.Marca propia ', es_marca_propia,'.Nombre proveedor ',ifnull(nombre_proveedor,''),'.Nombres matrículas: ', ifnull(nombre_matricula_nivel0,''), ', ', ifnull(nombre_matricula_nivel1,''), ', ', ifnull(nombre_matricula_nivel2,''), ', ', ifnull(nombre_matricula_nivel3,''), ', ', ifnull(nombre_matricula_nivel4,''), ', ', ifnull(nombre_matricula_nivel5,''), '. Información ', ifnull(txt_mas_informacion_del_producto,''), '. Instrucciones ', ifnull(txt_instrucciones_de_uso,''), '. Composición ', ifnull(txt_composicion,'')) AS content FROM `DataHub.Muestras_Cofares_Limpio` where id in (" + consulta + ")"
+    query = "SELECT nombre_material, concat('nombre ', ifnull(nombre_material,''), '.Nombre_largo ', ifnull(nombre_material_largo,''),'.Marca propia ', es_marca_propia,'.Nombre proveedor ',ifnull(nombre_proveedor,''),'.Nombres matrículas: ', ifnull(nombre_matricula_nivel0,''), ', ', ifnull(nombre_matricula_nivel1,''), ', ', ifnull(nombre_matricula_nivel2,''), ', ', ifnull(nombre_matricula_nivel3,''), ', ', ifnull(nombre_matricula_nivel4,''), ', ', ifnull(nombre_matricula_nivel5,''), '. Información ', ifnull(txt_mas_informacion_del_producto,''), '. Instrucciones ', ifnull(txt_instrucciones_de_uso,''), '. Composición ', ifnull(txt_composicion,'')) AS content FROM `DataHub.Muestras_Cofares_Limpio` where id in (" + consulta + ")"
     
     query_job = client.query(query)
     results = query_job.result().to_dataframe()
     
-    productos = "\n".join(results["nombre_material"])
     contexto += str(results["content"])
     client.close()
-    return contexto, productos
+    
+    return contexto
 
 def respuesta_promt(consulta,contexto):
     prompt = "Eres un ayudante de farmacia, tienes que conseguir hacer la pregunta correcta para saber que producto es mejor, teniendo en cuenta los productos relevantes y el contexto anterior, contesta únicamente con la pregunta que harías al usuario. La pregunta nunca puede ser sobre si le interesa tal o cual producto"
@@ -81,20 +95,19 @@ def respuesta_promt(consulta,contexto):
     prompt += "\nconsulta " + str(consulta)
     return generative_multimodal_model.generate_content(prompt)
 
-def buscar_producto(consulta: List[str], veto: List[int]) -> Tuple[List[float], str]:
+def buscar_producto(consulta: List[str], veto: List[int]) -> Tuple[List[dict], str]:
     # Generar el prompt para el sistema RAG
     data = gen_conv_KNN(consulta, veto)
 
-    cercanos = buscar_cercanos(data, veto)
-    contexto, productos = get_info(cercanos)
-    res = "\nProductos encontrados:" + productos
+    productos_encontrados, contexto = buscar_cercanos(data, veto)
     
-    respuesta = respuesta_promt(consulta, contexto)
-    res += respuesta.text
-    return res
+    respuesta = respuesta_promt(consulta, contexto).text
+
+    return productos_encontrados, respuesta
 
 # Inicializa Flask
 app = Flask(__name__)
+CORS(app) # Permitir solicitudes desde cualquier origen
 
 # Modifica la función main para que sea un endpoint
 @app.route('/consulta', methods=['POST'])
@@ -105,19 +118,29 @@ def chat_endpoint():
             return jsonify({'error': 'No se recibieron datos'}), 400
             
         user_input = data.get('message')
-        if not user_input:
-            return jsonify({'error': 'El mensaje está vacío'}), 400
+        pass_input = data.get('password')
 
-        respuesta = buscar_producto(user_input, [])
-        return jsonify({'response': respuesta})
+        # Comprobar contraseña
+        if not hashlib.sha256(pass_input.encode()).hexdigest() == "bcbe984a9013368372962bd78a4c16b1a2f67c38cb400ed8a9a3a2173712374c":
+            return jsonify({'Password': 'La contraseña es incorrecta'}), 400
+        
+        # Comprobar contraseña con mensaje vacío
+        if not user_input and hashlib.sha256(pass_input.encode()).hexdigest() == "bcbe984a9013368372962bd78a4c16b1a2f67c38cb400ed8a9a3a2173712374c":
+            return jsonify({'Password': 'La contraseña es correcta'}), 200
+
+        productos_encontrados, respuesta = buscar_producto(user_input, [])
+        
+        # Estructura de respuesta deseada
+        response = {
+            "Recomendaciones": productos_encontrados,
+            "Respuesta": respuesta
+        }
+        
+        return jsonify(response)
         
     except Exception as e:
-        print(f"Error: {str(e)}")  # Para logging
+        print(f"Error: {str(e)}")
         return jsonify({'error': f'Error interno del servidor: {str(e)}'}), 500
-
-@app.route('/', methods=['GET'])
-def hello():
-    return "¡Bienvenido a FarmaCIA!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
